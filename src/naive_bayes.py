0b8cb18e0abca583b14e16fa9080ffda521f40d8
nyasui <nikolaus.yasui@mail.mcgill.ca>
2017-02-04 17:44:59 -0500

    predicts class names

--- src/naive_bayes.py
+++ src/naive_bayes.py
@@ -17,17 +17,19 @@ def fit_nb(x, y, cols=None):
             classes[y[i]] = 1
             counts[y[i]] = x[i]
 
-    b = np.array([classes[y_i] for y_i in sorted(classes.keys())])/n
+    class_names = sorted(classes.keys())
+    b = np.array([classes[y_i] for y_i in class_names])/n
     
-    counts = np.array([counts[y_i] for y_i in sorted(classes.keys())])
+    counts = np.array([counts[y_i] for y_i in class_names])
     total_counts = np.sum(counts, axis=1)[np.newaxis].T
     w = (counts + 1)/(total_counts + alpha)
+
 
     def predict(x):
         if len(cols) < len(x[0]):
             x = x[:, cols]
-        return np.argmax(b + np.dot(x, w.T))[newaxis].T
+        indices = np.argmax(b + np.dot(x, w.T))
+        predictions = np.array([class_names[i] for i in indices])[newaxis].T
 
 # fit_cols_nb = lambda cols: partial(fit_nb, cols=cols)
-
 
