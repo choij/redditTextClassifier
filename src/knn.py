156926b32b4a2e647bf51edfd663f65d914c50e6
cmazzaanthony <cmazzaanthony@gmail.com>
2017-02-09 21:39:45 -0500

    refactored and ran first sample using bootstrap code

--- src/knn.py
+++ src/knn.py
@@ -1,45 +1,49 @@
 from FeatureEngineering import FeatureEngineering
+from bootstrap import BootstrapModel, Bootstrap
 import numpy as np
 import math
 import operator as op
+from tools import timit
+from collections import defaultdict
 from sklearn import cross_validation
 
-class KNN:
+class KNN(BootstrapModel):
 
-    def calcKNN(self, train, test=None, k=10):
-        # test = train[0]
-        d_list = []
-        for row in train:
-            t = row.tolist()[0]
-            d = self.calcEuDist(t,test.tolist()[0])
-            d_list.append((t,d))
+    def __init__(self):
+        super().__init__()
 
-        d_list.sort(key=op.itemgetter(1))
-        # print([(d[0][-1], d[-1]) for d in d_list])
-        n = [d_list[i][0] for i in range(k)]
+    @timit
+    def calcKNN(self, test=None, k=10):
+        d_list = defaultdict(lambda: 0)
+        for i in range(self.x_train.shape[0]):
+            label = self.y_train[i].item() #label e.g. news, politics
+            d = 1/np.linalg.norm(self.x_train[i].todense() - test.todense())
+            d_list[label] += d
 
-        # Sum euclidian distances instead of the below method.
-        popular = {}
-        for i in n:
-            neighbour = i[-1]
-            if neighbour in popular:
-                popular[neighbour] += 1
-            else:
-                popular[neighbour] = 1
+        d_list = sorted(d_list.items(), key=lambda k_v: k_v[1], reverse=True)
+        return d_list[0][0]
 
-        value = max(popular.items(), key=op.itemgetter(1))
-        return value
+    # def calcEuDist(self, x1, x2):
+    #     d = 0
+    #     x1 = x1[:-1]
+    #     x2 = x2[:-1]
+    #     for i in range(len(x1)):
+    #         d += pow(x1[i] - x2[i],2)
+    #     ed = math.sqrt(d)
+    #     return ed
 
-    def calcKernal(self, distance):
+    def fit(self, x, y):
+        self.x_train = x
+        self.y_train = y
+        self.class_names = np.unique(y)
+        self.fitted = True
 
-    def calcEuDist(self, x1, x2):
-        d = 0
-        x1 = x1[:-1]
-        x2 = x2[:-1]
-        for i in range(len(x1)):
-            d += pow(x1[i] - x2[i],2)
-        ed = math.sqrt(d)
-        return ed
+    def predict(self, x_test):
+        predictions = []
+        for row in x_test:
+            p = self.calcKNN(row)
+            predictions.append(p)
+        return np.array(predictions).reshape(-1,1)
 
     def predict_values(self, train_set, test_set, y_test):
         predictions = []
@@ -66,13 +70,17 @@ if __name__ == "__main__":
     x_ser = fe.read_x_train_features()
     y_mat = fe.read_y_train_features()
     x_mat = fe.calc_count_matrix(x_ser)
-    x_train = fe.merge_matrix(x_mat.todense(), y_mat)[:200, :]
-    x_test = fe.merge_matrix(x_mat.todense(), y_mat)[10:20, :]
+    # x_train = fe.merge_matrix(x_mat.todense(), y_mat)[:200, :]
+    # x_test = fe.merge_matrix(x_mat.todense(), y_mat)[10:20, :]
     y_test = y_mat[10:20, :]
-    knn = KNN()
+    # knn = KNN()
     # knn.calcKNN(x_y_train_mat)
+    loss = lambda y_hat, y: np.vectorize(int)(y_hat == y)
+    bootstrap = Bootstrap(x_mat, y_mat, loss, [KNN()], num_samples=10)
+    bootstrap.run()
+    bootstrap.print_summary()
 
-    scores = knn.predict_values(x_train, x_test, y_test)
+    # scores = knn.predict_values(x_train, x_test, y_test)
 
     data1 = [2, 2, 2, 'a']
     data2 = [4, 4, 4, 'b']
